f1af705ad1634a7479886526bd4f11540c617684
ALEXANDER ALBERTO SIGUENZA CAMPOS <alexander.siguenzac@claro.com.sv>
2024-09-20 19:22:49 -0600

    se actualizo la pantalla de login

--- screens/Auth/LoginScreen.js
+++ screens/Auth/LoginScreen.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import AuthForm from '../../components/AuthForm';
+import { useNavigation } from '@react-navigation/native';
 
 const LoginScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <View>
       <Text>Iniciar Sesión</Text>
       <AuthForm isLogin={true} />
+      <Button 
+        title="¿No tienes cuenta? Regístrate aquí"
+        onPress={() => navigation.navigate('Register')} 
+      />
     </View>
   );
 };
 
 export default LoginScreen;
+
+
--- screens/Auth/RegisterScreen.js
+++ screens/Auth/RegisterScreen.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import AuthForm from '../../components/AuthForm';
+import { useNavigation } from '@react-navigation/native';
 
 const RegisterScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <View>
       <Text>Registrarse</Text>
       <AuthForm isLogin={false} />
+      <Button 
+        title="¿Ya tienes una cuenta? Inicia sesión aquí"
+        onPress={() => navigation.navigate('Login')} 
+      />
     </View>
   );
 };
 
 export default RegisterScreen;
+
